8b72ced0bcbd341db93f8101fb897d2f720054c8
Yuanqing Wu <yuanqingwu@whu.edu.cn>
2021-12-10 12:13:55 +0800

    Fix a bug

--- src/network/loss.py
+++ src/network/loss.py
@@ -2,28 +2,21 @@ import torch
 
 
 class ContentLoss(torch.nn.Module):
-
     """Content Loss class"""
-
     def __init__(self, weight, target):
         """Init some settings
-
         :weight: weight matrix
         :target: Desire output
-
         """
         torch.nn.Module.__init__(self)
-
         self._weight = weight
         self._target = target.detach()*self._weight
         self.loss_fn = torch.nn.MSELoss()
 
     def forward(self, input):
         """Forward method for Content Loss Layer
-
         :input: input matrix
         :returns: output matrix
-
         """
         self.loss = self.loss_fn(input*self._weight, self._target)
         self.output = input
@@ -32,40 +25,32 @@ class ContentLoss(torch.nn.Module):
     def backward(self):
         """Backward method for Content Loss Layer
         :returns: TODO
-
         """
-        self.loss.backward(retain_graph = True)
+        self.loss.backward(retain_graph=True)
         return self.loss
 
 
-class GramMetrix(torch.nn.Module):
-
+class GramMatrix(torch.nn.Module):
     """Gram Matrix Class"""
-
     def __init__(self):
         """Do nothing """
         torch.nn.Module.__init__(self)
 
     def forward(self, input):
         """Forward method for gram matrix
-
         :input: matrix
         :returns: TODO
-
         """
-        a,b,c,d = input.size()
+        a, b, c, d = input.size()
         feature = input.view(a*b, c*d)
-        gram =torch.mm(feature, feature.t())
+        gram = torch.mm(feature, feature.t())
         return gram.div(a*b*c*d)
-        
+
 
 class StyleLoss(torch.nn.Module):
-
     """Style Loss Class"""
-
     def __init__(self, weight, target):
         """TODO: to be defined.
-
         :weight: weight matrix
         :target: Desire output
         """
@@ -73,17 +58,15 @@ class StyleLoss(torch.nn.Module):
         self._weight = weight
         self._target = target.detach()*self._weight
         self._loss_fn = torch.nn.MSELoss()
-        self._gram = GramMetrix()
-    
+        self._gram = GramMatrix()
+
     def forward(self, input):
         """Forward mehod for Style Loss
-
         :input: input matrix
         :returns: output matrix
-
         """
         self.output = input.clone()
-        self.G= self._gram(input)
+        self.G = self._gram(input)
         self.G.mul_(self._weight)
         self.loss = self._loss_fn(self.G, self._target)
         return self.output
@@ -91,7 +74,6 @@ class StyleLoss(torch.nn.Module):
     def backward(self):
         """Backward method for Style loss
         :returns: Loss matrix
-
         """
-        self.loss.backward(retain_graph = True)
+        self.loss.backward(retain_graph=True)
         return self.loss
